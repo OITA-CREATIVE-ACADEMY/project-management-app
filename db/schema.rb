322b254f57b0017061c923d24d9ac59603c5df97
TakuyaHarayama <44060633+harayama2@users.noreply.github.com>
2024-06-22 21:37:29 +0900

    プロジェクトページの骨組みを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_08_123623) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_22_121735) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "owner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_projects_on_owner_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
