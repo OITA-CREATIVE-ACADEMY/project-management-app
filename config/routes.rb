7a1b7675cd729761b5876ae977928d7a415adef9
TakuyaHarayama <44060633+harayama2@users.noreply.github.com>
2024-06-22 21:13:50 +0900

    ログインしているときはダッシュボードをルートにする * https://github.com/heartcombo/devise/wiki/How-To:-Define-a-different-root-route-for-logged-out-users

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resource :dashboard, controller: :dashboard, only: [:show]
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  authenticated :user do
+    root "dashboard#show", as: :user_root
+  end
+
   root "home#index"
 end
